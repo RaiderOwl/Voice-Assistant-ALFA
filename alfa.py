e439bbbd4833b6e3a9d06be36deb20e45b4e012d
RaiderOwl <akinbalturk@hotmail.com>
2024-01-07 04:09:33 +0300

    first commit

---
+++ alfa.py
@@ -0,0 +1,280 @@
+#Libraries
+import speech_recognition as sr
+from datetime import datetime
+from gtts import gTTS
+import sys
+import joblib
+import pyttsx3
+import pandas as pd
+import numpy as np
+import webbrowser
+import pywhatkit as kt
+import webbrowser as wb
+import time
+import pyautogui
+import keyboard
+import sounddevice as sd
+import requests
+import threading
+import subprocess
+import os 
+import psutil
+import json
+#Functions
+def log(message):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    log_message = f"[{current_time}] {message}"
+    print(log_message)
+
+def listen():
+    text = ""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:                # use the default microphone as the audio source
+        r.adjust_for_ambient_noise(source, duration=0.5) #adjust the energy threshold based on the surrounding noise level
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='tr-TR')
+            text = text.lower()
+            log(text)
+            if text == "kapan":
+                sys.exit()
+        except LookupError:                            # speech is unintelligible
+            log("Anlayamadım")
+            text = "error" 
+        except sr.UnknownValueError:
+            log("Bilinmeyen Hata")
+            text = "error"
+    return text
+
+def speak(audiostring):
+    log(audiostring)
+    engine.say(audiostring)
+    engine.runAndWait()
+
+def alfa(command):
+    speak("İşleme Alındı")
+    model = joblib.load("newsvcmodel.pkl")
+    vectorizer = joblib.load("newsvmvectorizer.pkl")
+
+    command_vector = vectorizer.transform([command])
+    category = model.predict(command_vector)
+    print(category)
+    return category
+
+def write_json(data, json_path):
+    with open(json_path, 'w') as json_file:
+            json.dump(data, json_file)
+
+def load_json(json_path):
+    with open(json_path, 'r') as json_file:
+        return json.load(json_file)
+    
+def append_json(data, json_path):
+    loaded_data = load_json(json_path)
+    loaded_data.append(data)
+    write_json(loaded_data,json_path)
+
+
+class Music():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+
+    def play_music(song_name):
+        base_url = "https://www.youtube.com/results?search_query=" + song_name
+        webbrowser.open(base_url)
+        time.sleep(5)
+        pyautogui.click(560, 324)
+        time.sleep(10)
+
+class Weather():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+    
+    def weather_info(city, country=''):
+        geocode_base__url = 'https://api.opencagedata.com/geocode/v1/json?'
+
+        geocode_params = {
+            'q': f'{city},{country}',
+            'key': "48a3fe66d9ab40e8a0a7210620ace1fa"
+        }
+
+        geocode_response = requests.get(geocode_base__url, params=geocode_params)
+        geocode_data = geocode_response.json()
+
+        if geocode_response.status_code == 200:
+            # Extract latitude and longitude
+            lat = geocode_data['results'][0]['geometry']['lat']
+            lon = geocode_data['results'][0]['geometry']['lng']
+            print(lat)
+            print(lon)
+        weather_base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+
+        weather_params = {
+        'lat': lat,
+        'lon': lon,
+        'appid': "1d47169fd917e33f11df6c1fb2d67f84",
+        'units': 'metric'  # You can change this to 'imperial' for Fahrenheit
+        }
+        
+
+        # Make the API request
+        weather_response = requests.get(weather_base_url, params=weather_params)
+        weather_data = weather_response.json()
+        print(weather_data)
+        # Check if the request was successful
+        if weather_response.status_code == 200:
+            # Extract relevant weather information
+            description = weather_data['weather'][0]['description']
+            temperature = weather_data['main']['temp']
+            humidity = weather_data['main']['humidity']
+
+            # Print or return the weather information
+            result = f'The weather in {city} is {description}. '
+            result += f'Temperature: {temperature}°C. Humidity: {humidity}%.'
+            print(result)
+
+class Date():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+
+    def today():
+        # Get the current date and format it
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        return current_date
+    
+    def special_days(day_name):
+        special_days = {
+            "yılbaşı": "1 ocak",
+            "ulusal egemenlik ve çocuk bayramı": "23 nisan",
+            "emek ve dayanışma günü": "1 mayıs",
+            "atatürk'ü anma, gençlik ve spor bayramı": "19 mayıs",
+            "zafer bayramı": "30 ağustos",
+            "cumhuriyet bayramı": "29 ekim",
+            "öğretmenler günü": "24 kasım",
+            "sevgililer günü": "14 şubat",
+            "dünya kadınlar günü": "8 mart",
+            "çanakkale zaferi": "18 mart",
+            "demokrasi ve milli birlik günü": "15 temmuz"
+        }
+        for key in special_days:
+            if day_name.lower() in key.lower():
+                event_date = special_days[key]
+                print(event_date)
+                return event_date
+
+class Time():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+    
+    def now():
+        current_time = datetime.now().strftime("%H:%M:%S")
+        return current_time
+    
+    def alarm(hour,minutes):
+        alarm_hour = hour
+        alarm_minute = minutes
+        alarm = 1
+        temp_time = Time.now()
+        while alarm == 1 and temp_time.second == "01":
+            current_time = datetime.now().strftime("%H:%M:%S")
+            if current_time.hour == alarm_hour and current_time.minute == alarm_minute:
+                speak("Alarm. Uyanın")
+                break
+            else:
+                time.sleep(60)  # Check every 60 seconds
+
+    def timer_callback():
+        speak("zaman doldu")
+
+    def timer(minutes=None, seconds=None):
+
+        duration_seconds = (minutes or 0) * 60 + (seconds or 0)
+        timer = threading.Timer(duration_seconds, Time.timer_callback)
+        timer.start()
+        
+class Programs():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+
+    def open(program_name):
+        json_file = load_json("program_data.json")
+
+        for program_data in json_file:
+            print(program_data)
+            if program_data['program_name'] == program_name:
+                 program_path = program_data['path']
+                 os.startfile(program_path)
+                 break
+
+    def close(program_name):
+        # Run the tasklist command to get a list of processes
+        result = subprocess.run(['tasklist'], stdout=subprocess.PIPE, text=True)
+        json_file = load_json("program_data.json")
+        for data in json_file:
+            if data['program_name'] == program_name:
+                 exe_name = data['exe_name']
+                 break
+        # Check if the program name is in the command output
+        if program_name.lower() in result.stdout.lower():
+            # Terminate the process
+            os.system(f'taskkill /im {exe_name}.exe /f')
+            print(f"Closed {program_name} successfully.")
+        else:
+            print(f"Program {program_name} not found.")
+
+    def append_program_info(program_name,path,exe_name):
+        data = {"program_name": program_name, "path": path, "exe_name": exe_name}
+        append_json(data,"program_data.json")
+
+class Calculation():
+    def __init__(self, name):
+        self.name = name
+        self.functions = {}
+
+# def do_actions(category):
+#     function_mapping = {
+#         1: function_1,  # Kategori 1 için fonksiyon
+#         2: function_2,  # Kategori 2 için fonksiyon
+#         3: function_3,  # Kategori 3 için fonksiyon
+#         4: function_4,  # Kategori 4 için fonksiyon
+#         5: function_5,  # Kategori 5 için fonksiyon
+#         6: function_6,  # Kategori 6 için fonksiyon
+#     }
+#     function_mapping[category]()
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)
+welcometext = "Merhaba Ben alfa"
+
+alfa("Steam uygulamasını açar mısın")
+print(3)
+alfa("6 ile 10 çarp")
+print(4)
+alfa("Note kapat")
+print(3)
+alfa("Bugün hava nasıl")
+print(1)
+alfa("Ankarada hava nasıl")
+print(1)
+alfa("sibel can çakmak çakmak çal")
+print(0)
+alfa("Elifin doğum günü ne ")
+print(2)
+alfa("3 kere 3")
+print(4)
+
+speak(welcometext)
+
+while True:
+    response = listen()
+    if response != "error" :
+        category = alfa(response)
+        #do_actions(category)
+
+
+
---
+++ generate_sentence.py
@@ -0,0 +1,365 @@
+import random
+import json
+import pandas as pd
+def generate_sentences(artist_names, song_names, verb_names, num_sentences):
+    generated_sentences = []
+
+    for _ in range(num_sentences):
+        artist = random.choice(artist_names)
+        song = random.choice(song_names)
+        verb = random.choice(verb_names)
+
+        generated_sentence =artist + " " + song + " " + verb
+        generated_sentences.append(generated_sentence)
+
+    return generated_sentences
+
+# Kullanıcı tarafından belirlenecek özellikler
+artist_names = [
+    "Ebru Gündeş", "Tarkan", "Sezen Aksu", "Sertab Erener", "Teoman",
+    "Hande Yener", "Kenan Doğulu", "Ajda Pekkan", "Cem Adrian", "Şebnem Ferah",
+    "Barış Manço", "Nilüfer", "Mor ve Ötesi", "Yalın",
+    "Müslüm Gürses", "İbrahim Tatlıses",  "Candan Erçetin", "Yüksek Sadakat",
+    "Gripin", "Athena", "Yıldız Tilbe", "Mor ve Ötesi","Duman",  "Cem Karaca", "Hayko Cepkin","Altay","Aşkın Nur Yengi","Atiye","Bengü","Bedük","Berkay","Demet Akalın","Emre Altuğ","Ferhat Göçer","Gökçe","Gökhan Türkmen",
+    "Göksel","Hadise","Gülsen","Simge Sağın","İrem Derici","Merve Özbey","Murat Boz","Sibel Can","Murat Dalkılıç","Mustafa Ceceli","Nil Karaibrahimgil","Serdar Ortaç",
+    "Yalın","Ziynet Sali"
+]
+
+song_names = [
+    "Abidik Gubidik Twist","Acem Kızı","Ada Sahillerinde Bekliyorum","Adem Olan Anlar","Adını Anmam","Ağlama Yar Ağlama","Ağrı Dağından Aştım",
+    "Ah Bir Ataş Ver","Akdeniz Akşamları","Aman Aman Nalbandım","Anası Kızından","Ankara Rüzgarı","Ankara'dan Abim Geldi","Ankara'nın Taşına Bak (Ankara türküsü)","Arım, Balım, Peteğim (şarkı)",
+    "Asmalı Mencere",
+    "Aşk Sakızı",
+    "Ayağında Kundura",
+    "Aysel Gürel tarafından yazılmış şarkılar listesi",
+    "Ayva Çiçek Açmış",
+    "Bafra oyun havası",
+    "Bak Bir Varmış Bir Yokmuş",
+    "Balad (albüm)",
+    "Bastım da Kırıldı İğdenin Dalı",
+    "Bedia...",
+    "Bekle Buğday Tanesi",
+    "Beklenen Şarkı (şarkı)",
+    "Ben Kalender Meşrebim Güzel Çirkin Aramam",
+    "Benzemez Kimse Sana",
+    "Bıçak Düşmez Belinden",
+    "Bir Dalda İki Kiraz",
+    "Bir İncecik Yolum Gider Gördes'e",
+    "Bir Of Çeksem Karşıki Dağlar Yıkılır (Kar Mı Yağmış)",
+    "Böyle Ayrılık Olmaz",
+    "Böyle Gelmiş Böyle Geçer Dünya",
+    "Bu Kalp Seni Unutur Mu",
+    "Cano Cano Cano",
+    "Cumartesi Türküsü",
+    "Çadırımın Üstüne",
+    "Çok Yaşa Sen Ayşe",
+    "Dağlarda Kar Olsaydım",
+    "Daha Senden Gayrı Aşık mı Yoktur",
+    "Dandini Dandini Dastana",
+    "Darıldın Mı Gülüm Bana",
+    "Der makam-ı Hüseyni Sakil-i Ağa Rıza",
+    "Der makam-ı Şüri Sema'i",
+    "Dillere Düşeceğiz Seninle",
+    "Dinle (Şebnem Paker albümü)",
+    "Dom Dom Kurşunu",
+    "Dört Peynirli Pizza (Hepsi şarkısı)",
+    "El Çek Tabip Sinem Üstünden",
+    "Elazığ Dik Halayı",
+    "Elbet Bir Gün Buluşacağız",
+    "Entarisi Ala Benziyor",
+    "Erkekler (şarkı)",
+    "Eşkıya Dünyaya Hükümdar Olmaz",
+    "Ezberbozan",
+    "Geçsin Günler, Haftalar, Aylar, Mevsimler, Yıllar",
+    "Gel Ayşem Gel Gezelim",
+    "Gel, Sen de Katıl Umudun Şarkısına!",
+    "Gemilerde Talim Var",
+    "Genç Osman Türküsü",
+    "Gözlerinin İçine Başka Hayal Girmesin",
+    "Gözünaydın",
+    "Hatırla Sevgili (şarkı)",
+    "Hava Nasıl Oralarda  (şarkı)",
+    "Hekimoğlu Derler Benim Aslıma",
+    "Hepsi Senin mi ",
+    "Her Şeye Rağmen (şarkı)",
+    "Hey Onbeşli",
+    "Hüp",
+    "İndim Havuz Başına",
+    "İzmir’in Kavakları",
+    "Kadifeden Kesesi",
+    "Kalbime Gömerim O Zaman",
+    "Kalpsizsin",
+    "Karam (Köprüler Yaptırdım Gelip Geçmeye)",
+    "Kâtibim",
+    "Katina'nın elinde makina",
+    "Keklik Gibi Kanadımı Süzmedim",
+    "Kız Çocuğu (şiir)",
+    "Kızılcıklar Oldu mu ",
+    "Kimseye Etmem Şikâyet",
+    "Kirlenmiş Çığlık",
+    "Konyalım",
+    "Küçük Ayşe (Küçük Asker)",
+    "Küçük Yaşta Aldım Sazı Elime",
+    "Mahur (şiir)",
+    "Mahur Peşrev (Gazi Giray Han)",
+    "Maraş'tan Bir Haber Geldi",
+    "Mavilim Uyanda Gel",
+    "Memleketim (şarkı)",
+    "Mendilim Allanıyor",
+    "Mendilimin Yeşili",
+    "Meşe Şarkısı",
+    "Müzik plakları dolduran Türk sinema oyuncuları",
+    "Ne Feryad Edersin Divane Bülbül",
+    "Nerden Gelirsin (Keklik)",
+    "O Şimdi Asker (şarkı)",
+    "On İkidir Aydın'ın Dermeni",
+    "Ormandan Gel",
+    "Ömrümce hep adım adım",
+    "Önsöz (şarkı)",
+    "Ramizem",
+    "Sabahın Seher Vaktinde",
+    "Saçları Kara Kız Kız (Podaraki)",
+    "Sana Güvenmiyorum (şarkı)",
+    "Sana Neler Edeceğim",
+    "Sarı Odalar",
+    "Sarı Saçlım Mavi Gözlüm",
+    "Sarı Zeybek",
+    "Sen beni unutmuşsun (Sakın dönme geriye)",
+    "Sen Bir Tanesin",
+    "Senden Ayrı Aylar Geçti Yıl Geçti (Dağlar)",
+    "Sevda Mıdır Yoksa",
+    "Sıra Sıra Siniler (Çanakkale türküsü)",
+    "Son Mektup (şarkı)",
+    "Şimdi Uzaklardasın",
+    "Şu Dağları Delmeli",
+    "Şu Gelen Atlı Mıdır (Damdan Düştüm Dalgalanıyorum)",
+    "Talihin Elinde Oyuncak Oldum",
+    "Tarkan Çakır - Z.A.C.",
+    "Tarkan Çakır (albüm)",
+    "Telgrafın Tellerine Kuşlar Mı Konar",
+    "Toz Pembe",
+    "Turna (türkü)",
+    "Türkiyem (şarkı)",
+    "Uçun Kuşlar Uçun İzmir'e Doğru",
+    "Uyan Sunam Uyan",
+    "Üç Kalp",
+    "Var Git Turnam",
+    "Vücud İkliminin Sultanı Sensin",
+    "Yallah Şoför",
+    "Yandım Tokat Yandım",
+    "Yaşasın Okulumuz",
+    "Yemen Türküsü",
+    "Yeni (şarkı)",
+    "Yolun Başı",
+    "Zeytinyağlı Yiyemem Aman",
+    "Zor Kadın"
+]
+
+verb_names = ["oynat","çal","aç"]
+
+# Toplamda kaç cümle oluşturmak istediğinizi belirleyin
+num_generated_sentences = 800
+
+# Cümleleri oluştur
+generated_sentences_for_music = generate_sentences(artist_names, song_names, verb_names, num_generated_sentences)
+
+# Oluşturulan cümleleri yazdır
+# for i, sentence in enumerate(generated_sentences_for_music, start=1):
+#     print(f"{i}. {sentence}")
+
+cities = [
+    "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya",
+    "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir",
+    "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur",
+    "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli",
+    "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum",
+    "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari",
+    "Hatay", "Isparta", "İçel (Mersin)", "İstanbul", "İzmir",
+    "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir",
+    "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa",
+    "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir",
+    "Niğde", "Ordu", "Rize", "Sakarya", "Samsun",
+    "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat",
+    "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van",
+    "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman",
+    "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan",
+    "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"
+]
+
+weather_question_templates = [
+    "Hava durumu nasıl şehir ismi",
+    "Şu anki hava durumu nedir şehir ismi",
+    "şehir ismi hava nasıl",
+    "Hava durumu tahminleri nedir şehir ismi",
+    "şehir ismi hava durumu raporu nedir",
+    "Bugün hava nasıl şehir ismi",
+    "Yarın hava durumu ne olacak şehir ismi",
+    "Hafta sonu için hava durumu tahminleri nedir şehir ismi"
+]
+
+generated_sentences_for_weather = []
+
+for _ in range(600):
+    city = random.choice(cities)
+    question_template = random.choice(weather_question_templates)
+    question = question_template.replace('şehir ismi', city)
+    generated_sentences_for_weather.append(question)
+
+# for i, sentence in enumerate(generated_sentences_for_weather, start=1):
+#    print(f"{i}. {sentence}")
+
+with open("special_day_data.json", 'r', encoding='utf-8') as json_file:
+    special_day_data = json.load(json_file)
+
+special_days = []
+
+for day in special_day_data:
+    special_days.append(day['day_name'])
+
+date_question_templates = [
+    "xgünü gününün tarihi nedir",
+    "xgünü günü ne zaman",
+    "xgünü günü"
+    "yisim doğum günü tarihi nedir",
+    "yisim doğum günü",
+    "yisim doğum günü ne zaman",
+    "yisim ne zaman doğdu"
+    "xgünü hangi tarihte",
+    "xgünü hangi günde kutlanır"
+]
+
+date_questions = [
+    "Bugünün tarihi nedir",
+    "Bugün günlerden ne",
+    "Hangi aydayız",
+    "Hangi gündeyiz",
+    "Bugün hangi gün",
+    "Yarın hangi gün ",
+    "Haftada kaç gün var",
+    "Sonraki ay ne",
+    "Yarın günlerden nedir",
+    "Yarın hangi gündü",
+    "Dün hangi tarihti",
+    "Dünün tarihi nedir",
+    "Yarının tarihi nedir",
+]
+
+turkish_names = [
+    "Ahmet", "Ayşe", "Mustafa", "Fatma", "Mehmet", "Zeynep", "Emine", "Ali", "Hüseyin", "Aysel",
+    "Hasan", "Hatice", "İbrahim", "Yusuf", "Şehrazat", "Cemal", "Gülay", "Ahmetcan", "Elif", "Ömer",
+    "Cemile", "Selim", "Zehra", "Murat", "Sevim", "Cem", "Selin", "Berkay", "Deniz", "Gizem",
+    "Sadiye", "Okan", "Tuğba", "Onur", "Serpil", "Serkan", "Asuman", "Volkan", "Nazlı", "Bilge",
+    "Halit", "Gamze", "İsmail", "Leyla", "Cihan", "Sibel", "Umut", "Gül", "Kerem", "Derya"
+]
+
+generated_sentences_for_date = []
+
+for dates in date_questions:
+    generated_sentences_for_date.append(dates)
+
+for _ in range(500):
+    question_template = random.choice(date_question_templates)
+
+    if "xgünü" in question_template:
+        day = random.choice(special_days)
+        question = question_template.replace('xgünü', day)
+    elif "yisim" in question_template:
+        name = random.choice(turkish_names)
+        question = question_template.replace('yisim', name)
+    
+    generated_sentences_for_date.append(question) 
+
+# for sentence in generated_sentences_for_date:
+#    print(sentence)
+
+program_names = [
+    "Word", "Excel", "PowerPoint", "Visual Studio", "Photoshop", "Illustrator", "Chrome", "Firefox",
+    "Sublime Text", "PyCharm", "Eclipse", "Unity", "Android Studio", "Atom", "Notepad++", "MATLAB",
+    "AutoCAD", "Blender", "VLC Media Player", "Zoom", "Teams", "Discord", "Git", "GitHub",
+    "InDesign", "Premiere Pro", "After Effects", "Maya", "3ds Max", "Safari", "Edge", "IntelliJ IDEA",
+    "NetBeans", "RStudio", "Xcode", "TensorFlow", "PyTorch", "Jupyter", "VS Code", "Brave", "FileZilla",
+    "WinRAR", "7-Zip", "MySQL Workbench", "Postman", "Cinema 4D", "Final Cut Pro", "Logic Pro"
+    # İhtiyacınıza göre program isimlerini ekleyebilirsiniz
+]
+
+program_sentence_templates = [
+    "x  uygulamasını aç",
+    "x  uygulamasını kapat",
+    "x  uygulamasını çalıştır",
+    "x  uygulamasını sonlandır",
+    "x aç",
+    "x kapat",
+    "x çalıştır",
+    "x sonlandır",
+    "x programını aç",
+    "x programını kapat",
+    "x programını çalıştır",
+    "x programını sonlandır"
+    # İhtiyacınıza göre cümle şablonlarını ekleyebilirsiniz
+]
+
+generated_sentences_for_program = []
+
+for _ in range(700):  # Artık sadece 10 cümle oluşturuyorum, sizin ihtiyacınıza göre arttırabilirsiniz
+    program_name = random.choice(program_names)
+    sentence_template = random.choice(program_sentence_templates)
+    sentence = sentence_template.replace('x', program_name)
+    generated_sentences_for_program.append(sentence)
+
+# for sentence in generated_sentences_for_program:
+#     print(sentence) 
+
+generated_sentences_for_calculation = []
+
+math_sentence_templates = [
+    "sayi1 ile sayi2'yi topla",
+    "sayi1'yi sayi2'ye ekle",
+    "sayi1'yi sayi2'den çıkar",
+    "sayi1 ile sayi2'yi çarp",
+    "sayi1'yi sayi2'ye böl",
+    "sayi1 ile sayi2'yi topla ve sonucu ekrana yaz",
+    "sayi1'yi sayi2'ye ekle ve sonucu göster",
+    "sayi1'yi sayi2'den çıkar ve sonucu yaz",
+    "sayi1 ile sayi2'yi çarp ve sonucu göster",
+    "sayi1'yi sayi2'ye böl ve sonucu yaz",
+    "sayi1'yi sayi2'ye böldüğümüzde kalan nedir",
+    "sayi1'yi sayi2'ye böl ve kalanı ekrana yaz",
+    "sayi1'yi sayi2'ye böl ve kalanı göster",
+    "sayi1'yi sayi2'ye böl ve kalanı yaz"
+    # İhtiyacınıza göre cümle şablonlarını ekleyebilirsiniz
+]
+
+
+for _ in range(450):  # Artık sadece 10 cümle oluşturuyorum, sizin ihtiyacınıza göre arttırabilirsiniz
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+
+    sentence_template = random.choice(math_sentence_templates)
+    sentence = sentence_template.replace('sayi1', str(x)).replace('sayi2', str(y))
+    generated_sentences_for_calculation.append(sentence)
+
+# Oluşturulan cümleleri ekrana yazdırma
+# for sentence in generated_sentences_for_calculation:
+#     print(sentence)
+    
+df = pd.DataFrame(columns=['sentence', 'label'])
+
+df = pd.concat([df, pd.DataFrame({'sentence': generated_sentences_for_music, 'label': '0'})], ignore_index=True)
+df = pd.concat([df, pd.DataFrame({'sentence': generated_sentences_for_weather, 'label': '1'})], ignore_index=True)
+df = pd.concat([df, pd.DataFrame({'sentence': generated_sentences_for_date, 'label': '2'})], ignore_index=True)
+df = pd.concat([df, pd.DataFrame({'sentence': generated_sentences_for_program, 'label': '3'})], ignore_index=True)
+df = pd.concat([df, pd.DataFrame({'sentence': generated_sentences_for_calculation, 'label': '4'})], ignore_index=True)
+
+
+df = df.sample(frac=1).reset_index(drop=True)
+
+print(df)
+
+df.to_csv('Data\data.csv', index=False)
+
+
+
+
+
+
+
